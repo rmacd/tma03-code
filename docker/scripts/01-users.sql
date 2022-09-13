e2c6c6dd7c6cd07f7f93e2e1916ddb92e0304688
Ronald MacDonald <ronald@rmacd.com>
2022-09-13 18:46:08 +0100

    initial commit

---
+++ docker/scripts/01-users.sql
@@ -0,0 +1,2 @@
+CREATE USER 'tma03'@'%' IDENTIFIED BY 'tma03-pass';
+GRANT ALL PRIVILEGES on tma03.* to 'tma03'@'%';
---
+++ src/main/java/com/rmacd/ou/repos/RegistrationRepo.java
@@ -0,0 +1,7 @@
+package com.rmacd.ou.repos;
+
+import models.generated.Registration;
+import org.springframework.data.repository.Repository;
+
+public interface RegistrationRepo extends Repository<Registration, String> {
+}
